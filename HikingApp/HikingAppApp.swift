6ac5add95c1f8cc668c8946103a3637581b07a42
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-06-13 14:39:15 +0900

    Initial Commit

---
+++ HikingApp/HikingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingAppApp.swift
+//  HikingApp
+//
+//  Created by Brandon Jones on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct HikingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
